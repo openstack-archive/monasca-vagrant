a46ec61d595f81c8a91487c1cbef971978266943
Craig Bryant <craig.bryant@hp.com>
2014-05-15 15:09:35 -0600

    Added new test notification_crud.py. Refactored other files for code reuse

--- tests/notification.py
+++ tests/notification.py
@@ -8,8 +8,8 @@ import json
 import subprocess
 
 
-def find_notifications(alarm_id):
-    args = ['sudo', 'cat', '/var/mail/root']
+def find_notifications(alarm_id, user):
+    args = ['sudo', 'cat', '/var/mail/' + user]
     result = []
     try:
         stdout = subprocess.check_output(args)
@@ -20,3 +20,30 @@ def find_notifications(alarm_id):
         if alarm_id in line:
             result.append(json.loads(line)['state'])
     return result
+
+
+def create(mon_client, name, email):
+    kwargs = {'name': name, 'address': email, 'type': 'EMAIL'}
+    result = mon_client.notifications.create(**kwargs)
+    return result['id']
+
+
+def update(mon_client, notification_id, name, email):
+    kwargs = {'id': notification_id, 'name': name, 'address': email,
+              'type': 'EMAIL'}
+    result = mon_client.notifications.update(**kwargs)
+    return result['id']
+
+
+def get(mon_client, notification_id):
+    kwargs = {'notification_id': notification_id}
+    result = mon_client.notifications.get(**kwargs)
+    return result
+
+
+def find_by_name(mon_client, name):
+    result = mon_client.notifications.list(**{})
+    for notification in result:
+        if notification['name'] == name:
+            return notification
+    return None
