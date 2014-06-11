dc5f0b062b00191e61e1dc873d6d7acdee1d39bc
Tim Kuhlman <tim.kuhlman@hp.com>
2014-06-11 10:07:12 -0600

    Added in a tenant for the mini-mon user

--- cookbooks/mini-mon/recipes/keystone.rb
+++ cookbooks/mini-mon/recipes/keystone.rb
@@ -25,10 +25,18 @@ except keystoneclient.exceptions:
     key = client.Client(token='ADMIN', endpoint='http://127.0.0.1:35357/v2.0/')
     user_list = key.users.list()
 
+# Create the tenant for mini-mon if it doesn't exist
+tenant_id = None
+for tenant in key.tenants.list():
+  if tenant.name == 'mini-mon':
+    tenant_id = tenant.id
+if tenant_id is None:
+  tenant_id = key.tenants.create('mini-mon').id
+
+# Create the user if it doesn't exist
 for user in user_list:
     if user.name == 'mini-mon':
         sys.exit(0)
-
-key.users.create(name='mini-mon', password='password', email='mini@mon.com', enabled=True)
+key.users.create(name='mini-mon', password='password', email='mini@mon.com', tenant_id=tenant_id, enabled=True)
 EOH
 end
