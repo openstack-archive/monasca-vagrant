3589a8d250812b96b3d36dc70ae23cfc3b55a52c
Tim Kuhlman <tim.kuhlman@hp.com>
2014-06-11 16:18:06 -0600

    Turns out the percona cookbook already writes a /root/.my.cnf

--- cookbooks/mini-mon/recipes/mysql_schema.rb
+++ cookbooks/mini-mon/recipes/mysql_schema.rb
@@ -25,17 +25,3 @@ cookbook_file '/var/lib/mysql/addr_validate.sql' do
   source 'addr_validate.sql'
   notifies :run, "bash[addr_validate_schema]"
 end
-
-# Write out for root a .my.cnf with the user/pass used by the mon-setup program
-# Note: The permissions below are quite insecure but this is sufficient for mini-mon
-directory '/root' do
-  action :create
-  mode 0755
-end
-file '/root/.my.cnf' do
-  action :create_if_missing
-  owner "root"
-  group "root"
-  mode "0644"
-  content "[client]\nuser=root\npassword=password"
-end
