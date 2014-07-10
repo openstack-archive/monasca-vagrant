e4f0ef3fe411643cff443dad2e8b7511d48f34c9
Tim Kuhlman <tim.kuhlman@hp.com>
2014-07-10 10:39:07 -0600

    Make sure influxdb is started before trying to create the db

--- cookbooks/mini-mon/recipes/influxdb.rb
+++ cookbooks/mini-mon/recipes/influxdb.rb
@@ -1,6 +1,10 @@
 # Create an influxdb database and users
 # Leverages the cookbook from https://github.com/SimpleFinance/chef-influxdb
 
+service 'influxdb' do
+  action :start
+end
+
 influxdb_database 'mon' do
   action :create
 end
