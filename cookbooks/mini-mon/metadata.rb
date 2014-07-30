2cc609367ca797f870c91740690b8231af2d1a40
David Schroeder <david.schroeder@hp.com>
2014-07-30 17:32:02 -0600

    Require python-mysqldb package on MySQL VM
    Change-Id: I7e3655877522ddfbd82b9aab7ec9f97dc77e2050

--- cookbooks/mini-mon/metadata.rb
+++ cookbooks/mini-mon/metadata.rb
@@ -4,5 +4,5 @@ maintainer_email "hpcs-mon@hp.com"
 license          "All rights reserved"
 description      "Base setup for all vagrant boxes"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.1.2"
 depends          "apt"
