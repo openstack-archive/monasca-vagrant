993e01a0c1ee713995611d6edfbc25d4d0434628
Derrick Johnson <derrick.johnson@hp.com>
2014-07-30 09:10:02 -0700

    updated the agent databag with new role for monasca-agent
    updated the split roles as well
    
    add user monasca-agent
    
    had to manually merge data_bags
    
    cleaned up data bag
    
    Change-Id: If5c22a16208d301b363514d9496da6c19252458e

--- cookbooks/devstack/recipes/keystone.rb
+++ cookbooks/devstack/recipes/keystone.rb
@@ -1,6 +1,6 @@
 # Sets up a user mini-mon username/password in keystone
 
-python 'make default keystone user' do
+python 'make default keystone users' do
   action :run
   code <<-EOH
 import keystoneclient
@@ -16,16 +16,39 @@ except keystoneclient.exceptions:
 
 # Create the tenant for mini-mon if it doesn't exist
 tenant_id = None
+monasca_agent_id = None
+
+#Need to create monasca-agent
+
 for tenant in key.tenants.list():
   if tenant.name == 'mini-mon':
     tenant_id = tenant.id
+  if monasca_agent_id == 'monasca_agent':
+    monasca_agent_id = tenant.id
+
 if tenant_id is None:
   tenant_id = key.tenants.create('mini-mon').id
 
+if monasca_agent_id is None:
+  monasca_agent_id = key.tenants.create('monasca-agent').id
+
+create_mini_mon_user = True
+create_monasca_agent_user = True
 # Create the user if it doesn't exist
 for user in user_list:
     if user.name == 'mini-mon':
-        sys.exit(0)
-key.users.create(name='mini-mon', password='password', email='mini@mon.com', tenant_id=tenant_id, enabled=True)
+        create_mini_mon_user = False
+        create_monasca_agent_user = False
+if create_mini_mon_user:
+    key.users.create(name='mini-mon', password='password', email='mini@mon.com', tenant_id=tenant_id, enabled=True)
+
+monasca_user = None
+if create_monasca_agent_user:
+    monasca_agent_user = key.users.create(name='monasca-agent', password='password', email='monasca-agent@mon.com', tenant_id=monasca_agent_id, enabled=True)
+
+#create role for monasca agent
+if create_monasca_agent_user:
+  role = key.roles.create('monasca-agent')
+  key.roles.add_user_role(monasca_agent_user, role, monasca_agent_id);
 EOH
 end
