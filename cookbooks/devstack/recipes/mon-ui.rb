ea0e7ded7d8d52284129fa7b5f35e1b4f78280bb
Rob Raymond <rob.raymond@hp.com>
2014-08-12 09:53:17 -0600

    Simplify condition expression to just gtep
    Change-Id: I31dc02c1968b8d40e71d7751ba7b2c68d21f8cd2

--- cookbooks/devstack/recipes/mon-ui.rb
+++ cookbooks/devstack/recipes/mon-ui.rb
@@ -14,8 +14,7 @@ if !::File.exists?("/opt/stack/horizon/monitoring")
     execute "ln -sfv /usr/local/share/monasca/ui/enabled/* /opt/stack/horizon/openstack_dashboard/local/enabled/"
     execute "ln -sv /usr/local/lib/python2.7/dist-packages/monitoring /opt/stack/horizon/monitoring"
 end
-if ::File.exists?("/usr/local/share/monasca/ui/local_settings.py") && 
-   ::File.readlines("/opt/stack/horizon/openstack_dashboard/local/local_settings.py").grep(/MONITORING_SERVICES/).size == 0
+if ::File.readlines("/opt/stack/horizon/openstack_dashboard/local/local_settings.py").grep(/MONITORING_SERVICES/).size == 0
     execute "cat /usr/local/share/monasca/ui/local_settings.py >> /opt/stack/horizon/openstack_dashboard/local/local_settings.py"
 end
 
