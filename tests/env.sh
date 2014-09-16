8af50971c3bcfe3e27d743ab5d81e809397f586e
Tim Kuhlman <tim.kuhlman@hp.com>
2014-09-16 15:31:38 -0600

    Added env variables for use with python-monascaclient
    Change-Id: If1034956b1e32f5aefbea6ec28e103bafe3664bd

---
+++ tests/env.sh
@@ -0,0 +1,8 @@
+# Environment variables for use with python-monascaclient running via monaca-vagrant
+
+export OS_USERNAME=mini-mon
+export OS_PASSWORD=password
+export OS_TENANT_NAME=mini-mon
+export OS_AUTH_URL=http://192.168.10.5:35357/v2.0/
+
+export MONASCA_API_URL=http://192.168.10.4:8080/v2.0/
