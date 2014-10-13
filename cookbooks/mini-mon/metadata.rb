8dffb78e2e090a807fe11c17c79d26095245c939
Tim Kuhlman <tim.kuhlman@hp.com>
2014-10-12 22:41:36 -0600

    Initial setup via Ansible.
    Change-Id: I8f24134a1c3410acb8bc12fa07023b8eb32be93f

--- cookbooks/mini-mon/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             "mini-mon"
-maintainer       "Mon Team"
-maintainer_email "hpcs-mon@hp.com"
-license          "All rights reserved"
-description      "Base setup for all vagrant boxes"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.3"
-depends          "apt"
