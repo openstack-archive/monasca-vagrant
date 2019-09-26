1d3aee82fb7b3db70f186c32298da1ac289a2810
Jim Rollenhagen <jim@jimrollenhagen.com>
2019-09-26 10:30:49 -0400

    Retire github mirror, repo moved to opendev

--- env.sh
+++
@@ -1,7 +0,0 @@
-# Environment variables for use with python-monascaclient running via monaca-vagrant
-
-. /opt/monasca/bin/activate
-export OS_USERNAME=mini-mon
-export OS_PASSWORD=password
-export OS_PROJECT_NAME=mini-mon
-export OS_AUTH_URL=http://192.168.10.5:35357/v3/
